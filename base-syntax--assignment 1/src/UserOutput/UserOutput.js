adbe36c4913627aad4f810d1b21913a79da22993
Sam Snarr <sssnarr1@gmail.com>
2020-07-31 15:36:25 -0400

    assignment 1

---
+++ base-syntax--assignment 1/src/UserOutput/UserOutput.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const userOutput = (props) => {
+  return (
+    <div>
+      <p>{props.username}</p>
+      <p>paragraph 1</p>
+      <p>paragraph 2</p>
+    </div>
+  );
+};
+
+export default userOutput;
