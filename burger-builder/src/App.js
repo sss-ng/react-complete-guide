bf10a75b9d656406361d1199220c461f62146207
Sam Snarr <sssnarr1@gmail.com>
2020-08-03 13:38:57 -0400

    added BurgerIngredients

---
+++ burger-builder/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import './App.module.css';
+
+import Layout from './components/Layout/Layout'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>
+            Cillum irure voluptate ea aliquip nostrud cupidatat esse est. Nulla enim fugiat consequat tempor cupidatat laborum mollit ex adipisicing occaecat culpa. Magna ipsum magna sint minim sint nulla adipisicing minim ex pariatur officia incididunt voluptate.
+          </p>
+
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+
+class BurgerBuilder extends Component {
+  render() {
+    return <Aux></Aux>;
+  }
+}
+
+export default BurgerBuilder;
